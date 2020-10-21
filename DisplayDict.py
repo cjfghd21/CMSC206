9676bba8ecefd45328679779b2c91c8b6682aa6a
HansTheIV <32128159+HansTheIV@users.noreply.github.com>
2020-10-21 16:53:31 -0400

    Updated display and removed unnecessary/useless code

--- DisplayDict.py
+++ DisplayDict.py
@@ -1,10 +1,6 @@
 import json
 import PySimpleGUI
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-from spotipy.oauth2 import SpotifyClientCredentials
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-library-read"))
 
 f = open("SpotifyDataDict.txt", 'r')
 dictionary = json.loads(f.read())
@@ -17,7 +13,4 @@ for i in range(1,201):
         print(" is in the key " + str(dictionary[str(i)]['key']))
     except KeyError:
         print(str(i) + " Error")
-print(sp.recommendation_genre_seeds())
-rec = {"indie", "alt-rock", "show-tunes", "rock", "r-n-b"}
-print(sp.recommendations(seed_genres=rec))
 
